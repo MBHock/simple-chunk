d77f2e567c1c41246260e76773ad2f39ecddd48a
Mojammal Hock <Mojammal.Hock@adesso.de>
2019-10-27 22:27:22 +0100

    some log info

--- src/main/java/de/hock/batch/processing/InfoJobListener.java
+++ src/main/java/de/hock/batch/processing/InfoJobListener.java
@@ -3,8 +3,13 @@ package de.hock.batch.processing;
 import javax.batch.api.listener.JobListener;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Named("InfoJobListener")
 public class InfoJobListener implements JobListener {
@@ -14,8 +19,13 @@ public class InfoJobListener implements JobListener {
 
     @Override
     public void beforeJob() throws Exception {
-        System.getProperties().entrySet().forEach(entry -> logger.log(Level.INFO, "Key={0}, value={1}", new Object[]{entry.getKey(), entry.getValue()}) );
+        String collect = System.getProperties().entrySet().stream().map(entry -> String.format("%s=%s", entry.getKey(), entry.getValue())).collect(Collectors.joining(System.lineSeparator()));
+
+        //                forEach(entry -> logger.log(Level.INFO, "Key={0}, value={1}", new Object[]{entry.getKey(), entry.getValue()}) );
         logger.log(Level.INFO, "The job is starting");
+        logger.log(Level.INFO, "Properties \n {0}", collect);
+        new InputStreamReader();
+
     }
 
     @Override
